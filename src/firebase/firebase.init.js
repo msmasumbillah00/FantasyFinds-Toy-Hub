3d388b507c95798c8ea8a8a726f1f010c4babc28
Md Masum Billah <mdmasumbillahms00@gmail.com>
2024-03-30 11:22:41 +0800

    modify firebase

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-<<<<<<< HEAD
+
     apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
@@ -14,16 +14,6 @@ const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
     appId: import.meta.env.VITE_FIREBASE_APPID
 };
-=======
-    apiKey:  import.meta.env.FIREBASE_APIKEY,
-    authDomain: import.meta.env.FIREBASE_AUTHDOMAIN,
-    projectId: import.meta.env.FIREBASE_PROJECTID,
-    storageBucket:import.meta.env.FIREBASE_STORAGEBUCKET,
-    messagingSenderId:import.meta.env.FIREBASE_MESSAGINGSENDERID,
-    appId: import.meta.env.FIREBASE_APPID
->>>>>>> d245b61a6694a913de13f36ac852c411ba814af4
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
